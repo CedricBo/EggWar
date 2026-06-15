1bbcb7a4c9d5ad50e224aa14f5a3e3278fda7028
Cedric Bouton <cedricbouton@outlook.fr>
2026-06-15 08:49:34 +0200

    Add keyboard and mouse control for selected building type state

--- src/lib/placing_building_plugin.rs
+++ src/lib/placing_building_plugin.rs
@@ -2,9 +2,18 @@ mod buildings;
 
 use bevy::{
     app::{Plugin, Update},
-    ecs::system::Res,
-    input::{ButtonInput, keyboard::Key},
-    state::{app::AppExtStates, state::States},
+    ecs::{
+        schedule::{IntoScheduleConfigs, common_conditions::resource_changed},
+        system::{Res, ResMut},
+    },
+    input::{
+        ButtonInput,
+        keyboard::{Key, KeyCode}, mouse::MouseButton,
+    },
+    state::{
+        app::AppExtStates,
+        state::{NextState, State, States},
+    },
 };
 use buildings::building::BuildingType;
 
@@ -21,11 +30,35 @@ impl Plugin for PlacingBuildingPlugin {
         app.insert_state(SelectedBuildingToPlace::None);
 
         app.add_systems(Update, change_selected_building_with_keyboard);
+        app.add_systems(
+            Update,
+            print_selected_on_change.run_if(resource_changed::<State<SelectedBuildingToPlace>>),
+        );
     }
 }
 
-fn change_selected_building_with_keyboard(keyboard_inputs: Res<ButtonInput<Key>>) {
-    for pressed in keyboard_inputs.get_just_pressed() {
-        println!("Pressed {:?}", pressed);
+fn change_selected_building_with_keyboard(
+    keyboard_inputs: Res<ButtonInput<KeyCode>>,
+    mouse_inputs: Res<ButtonInput<MouseButton>>,
+    mut selected: ResMut<NextState<SelectedBuildingToPlace>>,
+) {
+    if keyboard_inputs.just_pressed(KeyCode::Digit1) {
+        selected.set(SelectedBuildingToPlace::Selected(BuildingType::Grange));
     }
+
+    if keyboard_inputs.just_pressed(KeyCode::Digit2) {
+        selected.set(SelectedBuildingToPlace::Selected(BuildingType::Garden));
+    }
+
+    if keyboard_inputs.just_pressed(KeyCode::Digit3) {
+        selected.set(SelectedBuildingToPlace::Selected(BuildingType::Turret));
+    }
+
+    if keyboard_inputs.just_pressed(KeyCode::Escape) || mouse_inputs.just_pressed(MouseButton::Right) {
+        selected.set(SelectedBuildingToPlace::None);
+    }
+}
+
+fn print_selected_on_change(state: Res<State<SelectedBuildingToPlace>>) {
+    println!("{:?}", state);
 }
